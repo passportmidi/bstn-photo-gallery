f10ac1223168327e371b7be50f0022f1c0690f5d
Sydney Putnam <sydney.putnam@proton.me>
2025-02-15 17:39:20 -0400

    style photographer label and image sizing

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,14 @@
 import "./Card.scss";
-import placeholderImage from "../../assets/images/Facebook.svg";
+import placeholderImage from "../../assets/images/Photo-00.png";
 
 function Card() {
   return (
     <div className="card">
       <div className="card__content">
         <img className="card__image" src={placeholderImage} alt="placeholder" />
-        <div className="card__photographer">Yaaaay</div>
+        <div className="card__photographer label">Yaaaay</div>
       </div>
-      <div className="card__tags">
+      <div className="card__tags label">
         <p>Placeholder</p>
       </div>
     </div>
