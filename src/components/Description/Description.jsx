c893f62d9979bbc6cd12f5a8896b38ce5d5afbc6
Sydney Putnam <sydney.putnam@proton.me>
2025-02-14 22:32:40 -0400

    tweak body font colour and font weight of 'Our mission:' title

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -3,7 +3,7 @@ import "./Description.scss";
 export default function Description() {
   return (
     <section className="desc">
-      <p className="desc__heading body-copy">Our mission:</p>
+      <p className="desc__heading body-copy body-copy--light">Our mission:</p>
       <p className="desc__body title">
         Provide photographers a space to share photos of the neighborhoods they
         cherish,
