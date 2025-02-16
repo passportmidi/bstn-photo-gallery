ddcc65cf3396a69cd876aacfdb35307745fe6f8d
Sydney Putnam <sydney.putnam@proton.me>
2025-02-16 17:28:35 -0400

    create Filter icon as component and add to button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import "./Header.scss";
+import Filter from "../Filter/Filter";
 
 export default function Header() {
   return (
     <header className="header">
       <h2 className="wordmark header__wordmark">Snaps</h2>
+      <button>
+        Filters <Filter fill="#1E6655" />
+      </button>
     </header>
   );
 }
