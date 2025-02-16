2fbbff786b1308f6e3871eb3177272b959f9437d
Sydney Putnam <sydney.putnam@proton.me>
2025-02-16 19:33:31 -0400

    change filter icon colour on hover

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import "./Header.scss";
 import Filter from "../Filter/Filter";
 
 export default function Header() {
+  const [hover, setHover] = useState(false);
+
   return (
     <header className="header">
       <h2 className="wordmark header__wordmark">Snaps</h2>
-      <button className="header__cta label">
-        Filters <Filter fill="#1E6655" />
+      <button
+        className="header__cta label"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        Filters <Filter fill={hover ? "white" : "#49675A"} />
       </button>
     </header>
   );
