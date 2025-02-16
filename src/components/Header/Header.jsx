4d1054a66d2ba532319c7de95b072ef85dff9859
Sydney Putnam <sydney.putnam@proton.me>
2025-02-16 19:19:23 -0400

    add iconFill param to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import "./Header.scss";
 import Filter from "../Filter/Filter";
 
-export default function Header() {
+export default function Header({ iconFill }) {
   return (
     <header className="header">
       <h2 className="wordmark header__wordmark">Snaps</h2>
       <button className="header__cta label">
-        Filters <Filter fill="#1E6655" />
+        Filters <Filter fill={iconFill} />
       </button>
     </header>
   );
