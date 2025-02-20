9d1b661eec80cca5b4f1b749715d4cc9e8a0ea70
Sydney Putnam <sydney.putnam@proton.me>
2025-02-20 18:34:19 -0400

    add routing for Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
-import { useState } from "react";
-import Header from "./components/Header/Header";
-import Drawer from "./components/Drawer/Drawer";
-import Description from "./components/Description/Description";
-import Card from "./components/Card/Card";
-import Footer from "./components/Footer/Footer";
-import photos from "./data/photos.json";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
 import "./App.scss";
-import { selectedTagContext, setSelectedTagContext } from "./TagContext";
 
 function App() {
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const [selectedTag, setSelectedTag] = useState(null);
-
-  const toggleDrawer = () => {
-    setDrawerOpen(!drawerOpen);
-  };
-
-  const filteredPhotos = selectedTag
-    ? photos.filter((photo) => photo.tags.includes(selectedTag))
-    : photos;
-
-  const cardsList = filteredPhotos.map((photo) => {
-    return <Card photo={photo} key={photo.id} />;
-  });
-
   return (
-    <>
-      <Header toggleFunction={toggleDrawer} />
-      <selectedTagContext.Provider value={selectedTag}>
-        <setSelectedTagContext.Provider value={setSelectedTag}>
-          <div className="app__body">
-            <Drawer className={`drawer${drawerOpen ? " drawer--open" : ""}`} />
-            <div className="app__content">
-              <Description />
-              <div className="app__cards">{cardsList}</div>
-            </div>
-          </div>
-        </setSelectedTagContext.Provider>
-      </selectedTagContext.Provider>
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { selectedTagContext, setSelectedTagContext } from "../TagContext";
+import Header from "../components/Header/Header";
+import Drawer from "../components/Drawer/Drawer";
+import Description from "../components/Description/Description";
+import Card from "../components/Card/Card";
+import Footer from "../components/Footer/Footer";
+import photos from "../data/photos.json";
+import "./Home.scss";
+
+function Home() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [selectedTag, setSelectedTag] = useState(null);
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const filteredPhotos = selectedTag
+    ? photos.filter((photo) => photo.tags.includes(selectedTag))
+    : photos;
+
+  const cardsList = filteredPhotos.map((photo) => {
+    return <Card photo={photo} key={photo.id} />;
+  });
+
+  return (
+    <>
+      <Header toggleFunction={toggleDrawer} />
+      <selectedTagContext.Provider value={selectedTag}>
+        <setSelectedTagContext.Provider value={setSelectedTag}>
+          <div className="app__body">
+            <Drawer className={`drawer${drawerOpen ? " drawer--open" : ""}`} />
+            <div className="app__content">
+              <Description />
+              <div className="app__cards">{cardsList}</div>
+            </div>
+          </div>
+        </setSelectedTagContext.Provider>
+      </selectedTagContext.Provider>
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
