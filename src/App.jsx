e5e90862019a1229f6af5127512472dcc2c9b335
Sydney Putnam <sydney.putnam@proton.me>
2025-02-21 09:19:19 -0400

    add placeholder Photo page and routing to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Photo from "./pages/Photo/Photo";
 import "./App.scss";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/photo/:photoId" element={<Photo />} />
         <Route path="home" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Photo/Photo.jsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import "./Photo.scss";
+
+function Photo() {
+  return (
+    <>
+      <Header />
+      <p className="body-copy">Hi there</p>
+      <Footer />
+    </>
+  );
+}
+
+export default Photo;
