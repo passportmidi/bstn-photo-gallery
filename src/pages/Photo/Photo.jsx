2aa6aa3f05aff55023c2bd350e16efaf8bcf7490
Sydney Putnam <sydney.putnam@proton.me>
2025-02-22 17:59:46 -0400

    add photo card to photo page

--- src/pages/Photo/Photo.jsx
+++ src/pages/Photo/Photo.jsx
@@ -1,12 +1,33 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Header from "../../components/Header/Header";
+import Card from "../../components/Card/Card";
 import Footer from "../../components/Footer/Footer";
 import "./Photo.scss";
 
 function Photo() {
+  const { photoId } = useParams();
+  const [photo, setPhoto] = useState(null);
+  const API_KEY = "8e3792a3-b23c-4f9d-97c2-c1e35ad2df23";
+
+  useEffect(() => {
+    const fetchPhoto = async () => {
+      const response = await axios.get(
+        `https://unit-3-project-c5faaab51857.herokuapp.com/photos/${photoId}?api_key=${API_KEY}`
+      );
+      setPhoto(response.data);
+    };
+    fetchPhoto();
+  }, [photoId]);
+
+  while (!photo) {
+    return <div>loading...</div>;
+  }
   return (
     <>
       <Header />
-      <p className="body-copy">Hi there</p>
+      <Card photo={photo} isHomePage={false} />
       <Footer />
     </>
   );
