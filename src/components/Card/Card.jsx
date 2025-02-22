40a37205e96147e65db307a0842d151319640348
Sydney Putnam <sydney.putnam@proton.me>
2025-02-21 21:09:47 -0400

    add routing to photos pages

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,28 @@
 import "./Card.scss";
 import Tag from "../Tag/Tag";
+import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { selectedTagContext, setSelectedTagContext } from "../../TagContext";
 
-function Card({ photo }) {
+function Card({ photo, onClickFunction }) {
   const selectedTag = useContext(selectedTagContext);
   const tagList = photo.tags.map((tag) => (
     <Tag value={tag} key={tag} selectedTag={selectedTag} />
   ));
   return (
-    <div className="card">
-      <div className="card__content">
-        <img
-          className="card__image"
-          src={photo.photo}
-          alt={photo.photoDescription}
-        />
-        <div className="card__photographer label">{photo.photographer}</div>
+    <Link to={`/photo/${photo.id}`}>
+      <div className="card">
+        <div className="card__content">
+          <img
+            className="card__image"
+            src={photo.photo}
+            alt={photo.photoDescription}
+          />
+          <div className="card__photographer label">{photo.photographer}</div>
+        </div>
+        <div className="card__tags">{tagList}</div>
       </div>
-      <div className="card__tags">{tagList}</div>
-    </div>
+    </Link>
   );
 }
 
