8cc58aeefe2d609456034c13cd961e69d89dc5b9
Sydney Putnam <sydney.putnam@proton.me>
2025-02-23 18:39:04 -0400

    add url variables for Photo.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 
 export default function Form() {
   const [name, setName] = useState(localStorage.name || "");
--- src/pages/Photo/Photo.jsx
+++ src/pages/Photo/Photo.jsx
@@ -12,12 +12,14 @@ export default function Photo() {
   const { photoId } = useParams();
   const [photo, setPhoto] = useState(null);
   const [comments, setComments] = useState(null);
+
+  const BASE_URL = "https://unit-3-project-c5faaab51857.herokuapp.com";
   const API_KEY = "8e3792a3-b23c-4f9d-97c2-c1e35ad2df23";
 
   useEffect(() => {
     const fetchPhoto = async () => {
       const response = await axios.get(
-        `https://unit-3-project-c5faaab51857.herokuapp.com/photos/${photoId}?api_key=${API_KEY}`
+        `${BASE_URL}/photos/${photoId}?api_key=${API_KEY}`
       );
       setPhoto(response.data);
     };
@@ -27,7 +29,7 @@ export default function Photo() {
   useEffect(() => {
     const fetchComments = async () => {
       const response = await axios.get(
-        `https://unit-3-project-c5faaab51857.herokuapp.com/photos/${photoId}/comments?api_key=${API_KEY}`
+        `${BASE_URL}/photos/${photoId}/comments?api_key=${API_KEY}`
       );
       setComments(response.data);
     };
