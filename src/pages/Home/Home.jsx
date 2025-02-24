8f293d01cf20470bdffc711dc44dbe50164fa222
Sydney Putnam <sydney.putnam@proton.me>
2025-02-23 22:30:13 -0400

    pull photos from API on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { selectedTagContext, setSelectedTagContext } from "../../TagContext";
 import { Link } from "react-router-dom";
 import Header from "../../components/Header/Header";
@@ -6,22 +6,38 @@ import Drawer from "../../components/Drawer/Drawer";
 import Description from "../../components/Description/Description";
 import Card from "../../components/Card/Card";
 import Footer from "../../components/Footer/Footer";
-import photos from "../../data/photos.json";
+import axios from "axios";
 import "./Home.scss";
 
 function Home() {
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [selectedTag, setSelectedTag] = useState(null);
+  const [photos, setPhotos] = useState(null);
+
+  const BASE_URL = "https://unit-3-project-c5faaab51857.herokuapp.com";
+  const API_KEY = "8e3792a3-b23c-4f9d-97c2-c1e35ad2df23";
+
+  useEffect(() => {
+    fetchPhotos();
+  }, [photos]);
+
+  async function fetchPhotos() {
+    const response = await axios.get(`${BASE_URL}/photos?api_key=${API_KEY}`);
+    const filteredPhotos = selectedTag
+      ? response.data.filter((photo) => photo.tags.includes(selectedTag))
+      : response.data;
+    setPhotos(filteredPhotos);
+  }
 
   const toggleDrawer = () => {
     setDrawerOpen(!drawerOpen);
   };
 
-  const filteredPhotos = selectedTag
-    ? photos.filter((photo) => photo.tags.includes(selectedTag))
-    : photos;
+  while (!photos) {
+    return <div>loading...</div>;
+  }
 
-  const cardsList = filteredPhotos.map((photo) => {
+  const cardsList = photos.map((photo) => {
     return (
       <Link to={`/photo/${photo.id}`} key={photo.id}>
         <Card photo={photo} isHomePage={true} font="label" />
